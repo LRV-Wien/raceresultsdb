47f83dc57f28713400b3f87ec30148d1b984e91f
Michael Kröll <michael.kroell@geizhals.at>
2019-05-21 18:48:56 +0200

    Added Elite/Amateure CX and ÖSTM/ÖM Bahn 2019 results

--- sql/lrvwien_meister2019-data.sql
+++ sql/lrvwien_meister2019-data.sql
@@ -17,5 +17,65 @@ COPY race_results (rider_id, pos, racetype_id, category_national, raceday_year, 
 10035024155	6	24	STRASSE MASTERS II	2019	http://www.rc-awr.at/index.php?id=71	ÖM Querfeldein 2019 Masters II	ÖM Querfeldein 2019
 10087646857	1	24	STRASSE MASTERS III	2019	http://www.rc-awr.at/index.php?id=71	ÖM Querfeldein 2019 Masters III	ÖM Querfeldein 2019
 10035021529	1	24	STRASSE MASTERS IV	2019	http://www.rc-awr.at/index.php?id=71	ÖM Querfeldein 2019 Masters IV	ÖM Querfeldein 2019
+10048381560	1	24	STRASSE ELITE	2019	http://www.rc-awr.at/index.php?id=71	ÖSTM Querfeldein 2019 ME	ÖSTM Querfeldein 2019
+10010610164	2	24	STRASSE ELITE	2019	http://www.rc-awr.at/index.php?id=71	ÖSTM Querfeldein 2019 ME	ÖSTM Querfeldein 2019
+10010610164	1	24	STRASSE U23	2019	http://www.rc-awr.at/index.php?id=71	ÖSTM Querfeldein 2019 MU	ÖSTM Querfeldein 2019
+10007193441	1	24	STRASSE AMATEURE	2019	http://www.rc-awr.at/index.php?id=71	ÖM Querfeldein 2019 Amateure	ÖM Querfeldein 2019
+10011376868	2	24	STRASSE AMATEURE	2019	http://www.rc-awr.at/index.php?id=71	ÖM Querfeldein 2019 Amateure	ÖM Querfeldein 2019
+10035025973	3	24	STRASSE AMATEURE	2019	http://www.rc-awr.at/index.php?id=71	ÖM Querfeldein 2019 Amateure	ÖM Querfeldein 2019
+10035019913	4	24	STRASSE AMATEURE	2019	http://www.rc-awr.at/index.php?id=71	ÖM Querfeldein 2019 Amateure	ÖM Querfeldein 2019
+10008623886	1	24	STRASSE FRAUEN	2019	http://www.rc-awr.at/index.php?id=71	ÖSTM Querfeldein 2019 WE	ÖSTM Querfeldein 2019
+10035026882	1	17	STRASSE U17	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Scratch MU17	ÖSTM/ÖM Bahn 2019
+10035026882	1	21	STRASSE U17	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Sprint MU17	ÖSTM/ÖM Bahn 2019
+10035026882	1	19	STRASSE U17	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Ausscheidungsrennen MU17	ÖSTM/ÖM Bahn 2019
+10035026882	1	20	STRASSE U17	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Einzelverfolgung MU17	ÖSTM/ÖM Bahn 2019
+10035026882	1	16	STRASSE U17	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Punkterennen MU17	ÖSTM/ÖM Bahn 2019
+10035026882	1	23	STRASSE U17	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Zeitfahren MU17	ÖSTM/ÖM Bahn 2019
+10035026882	1	22	STRASSE U17	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Keirin MU17	ÖSTM/ÖM Bahn 2019
+10035026882	1	18	STRASSE U17	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Temporennen MU17	ÖSTM/ÖM Bahn 2019
+10053587935	1	23	STRASSE AMATEURE	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Zeitfahren Amateure	ÖSTM/ÖM Bahn 2019
+10035019913	2	23	STRASSE AMATEURE	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Zeitfahren Amateure	ÖSTM/ÖM Bahn 2019
+10092046415	1	23	STRASSE ELITE	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Zeitfahren ME	ÖSTM/ÖM Bahn 2019
+10035022741	1	23	STRASSE JUNIOREN	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Zeitfahren MJ	ÖSTM/ÖM Bahn 2019
+10065666960	1	23	STRASSE MASTERS I	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Zeitfahren MM	ÖSTM/ÖM Bahn 2019
+10035022842	2	23	STRASSE MASTERS I	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Zeitfahren MM	ÖSTM/ÖM Bahn 2019
+10035029916	3	23	STRASSE MASTERS II	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Zeitfahren MM	ÖSTM/ÖM Bahn 2019
+10035019408	4	23	STRASSE MASTERS II	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Zeitfahren MM	ÖSTM/ÖM Bahn 2019
+10035021529	5	23	STRASSE MASTERS IV	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Zeitfahren MM	ÖSTM/ÖM Bahn 2019
+10035089833	6	23	STRASSE MASTERS III	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Zeitfahren MM	ÖSTM/ÖM Bahn 2019
+10035025670	1	23	STRASSE FRAUEN	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Zeitfahren WE	ÖSTM/ÖM Bahn 2019
+10035025569	2	23	STRASSE JUNIORINNEN	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Zeitfahren WE	ÖSTM/ÖM Bahn 2019
+10035025569	1	23	STRASSE JUNIORINNEN	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Zeitfahren WJ	ÖSTM/ÖM Bahn 2019
+10075353725	1	17	STRASSE U13	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Scratch MU13	ÖSTM/ÖM Bahn 2019
+10087643322	2	17	STRASSE U13	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Scratch MU13	ÖSTM/ÖM Bahn 2019
+10053587935	1	21	STRASSE AMATEURE	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Sprint ME	ÖSTM/ÖM Bahn 2019
+10035022741	2	21	STRASSE JUNIOREN	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Sprint ME	ÖSTM/ÖM Bahn 2019
+10092046415	3	21	STRASSE ELITE	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Sprint ME	ÖSTM/ÖM Bahn 2019
+10035022741	1	21	STRASSE JUNIOREN	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Sprint MJ	ÖSTM/ÖM Bahn 2019
+10065666960	1	21	STRASSE MASTERS I	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Sprint MM	ÖSTM/ÖM Bahn 2019
+10035029916	2	21	STRASSE MASTERS II	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Sprint MM	ÖSTM/ÖM Bahn 2019
+10035019408	3	21	STRASSE MASTERS II	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Sprint MM	ÖSTM/ÖM Bahn 2019
+10035022842	4	21	STRASSE MASTERS I	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Sprint MM	ÖSTM/ÖM Bahn 2019
+10035019307	5	21	STRASSE MASTERS II	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Sprint MM	ÖSTM/ÖM Bahn 2019
+10035089833	6	21	STRASSE MASTERS III	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Sprint MM	ÖSTM/ÖM Bahn 2019
+10035025670	1	21	STRASSE FRAUEN	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Sprint WE	ÖSTM/ÖM Bahn 2019
+10035025569	2	21	STRASSE JUNIORINNEN	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Sprint WE	ÖSTM/ÖM Bahn 2019
+10035025569	1	21	STRASSE JUNIORINNEN	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Sprint WJ	ÖSTM/ÖM Bahn 2019
+10087643322	1	20	STRASSE U13	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Einzelverfolgung MU13	ÖSTM/ÖM Bahn 2019
+10087643322	1	23	STRASSE U13	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Zeitfahren MU13	ÖSTM/ÖM Bahn 2019
+10053587935	1	22	STRASSE AMATEURE	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Keirin Amateure	ÖSTM/ÖM Bahn 2019
+10035019913	2	22	STRASSE AMATEURE	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Keirin Amateure	ÖSTM/ÖM Bahn 2019
+10053587935	1	22	STRASSE AMATEURE	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Keirin ME	ÖSTM/ÖM Bahn 2019
+10092046415	2	22	STRASSE ELITE	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Keirin ME	ÖSTM/ÖM Bahn 2019
+10035019913	3	22	STRASSE AMATEURE	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Keirin ME	ÖSTM/ÖM Bahn 2019
+10035022741	4	22	STRASSE JUNIOREN	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Keirin ME	ÖSTM/ÖM Bahn 2019
+10035022741	1	22	STRASSE JUNIOREN	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖM Bahn 2019 Keirin MJ	ÖSTM/ÖM Bahn 2019
+10065666960	1	22	STRASSE MASTERS I	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Keirin MM	ÖSTM/ÖM Bahn 2019
+10035019408	2	22	STRASSE MASTERS II	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Keirin MM	ÖSTM/ÖM Bahn 2019
+10048896670	3	22	STRASSE MASTERS II	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Keirin MM	ÖSTM/ÖM Bahn 2019
+10035029916	4	22	STRASSE MASTERS II	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Keirin MM	ÖSTM/ÖM Bahn 2019
+10035022842	5	22	STRASSE MASTERS I	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Keirin MM	ÖSTM/ÖM Bahn 2019
+10035019307	6	22	STRASSE MASTERS II	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Keirin MM	ÖSTM/ÖM Bahn 2019
+10035089833	7	22	STRASSE MASTERS III	2019	https://www.radsportverband.at/index.php/portale/bahn/allgemeines-bahn	ÖSTM Bahn 2019 Keirin MM	ÖSTM/ÖM Bahn 2019
 \.
 
